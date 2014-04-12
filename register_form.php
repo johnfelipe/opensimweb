9207679a1a31ed1d326759143c1e6c3ba5efaa76
Chrisx84 <chrisx84@live.ca>
2014-04-12 14:38:31 -0400

    Registration and logout fixed
    Users can now logout, however their session info is still stored in the
    site database, will figure this out soon. Registration form fixed and
    loading perfectly however the actual registration process has not been
    tested yet.

--- register_form.php
+++ register_form.php
@@ -3,15 +3,11 @@ if (!defined('OSW_IN_SYSTEM')) {
 exit;
 }
 ?>
-<fieldset>
-	<legend>
-		Register
-	</legend>
 	<form action="register.php" method="post">
 		<input type="hidden" name="process" value="true" />
 <div class="container">
  <div class="row">
-  <div class="col-md-6">
+  <div class="col-md-12">
    <div class="panel panel-default">
     <div class="panel-heading"><i class="fa fa-lock"></i> Register</div>
     <div class="panel-body">
@@ -19,36 +15,35 @@ exit;
 	<div class="form-group">
 		<label for="InputFirst" class="col-sm-3 control-label">Avatar First Name</label>
 		<div class="col-sm-9">
-		<input type="text" id="InputFirst" name="firstname" maxlength="100" value="" required />
+		<input type="text" id="InputFirst" class="form-control" name="firstname" maxlength="100" value="" required />
 		</div>
 	</div>
 
 	<div class="form-group">
 		<label for="InputLast" class="col-sm-3 control-label">Avatar Last Name</label>
 		<div class="col-sm-9">
-		<input type="text" id="InputLast" name="lastname" maxlength="100" value="" required />
+		<input type="text" id="InputLast" class="form-control" name="lastname" maxlength="100" value="" required />
 		</div>
 	</div>
 
 	<div class="form-group">
-		<label for="InputPassword" class="col-sm-3 control-label">Password</label>
+		<label for="InputPassword" class="col-sm-3 control-label">Password <font color="Red">*</font></label>
 		<div class="col-sm-9">
-		<input type="password" id="InputPassword" name="password" maxlength="<?php echo $osw->config['max_password']; ?>" required />
-			<?php echo $osw->config['min_password']; ?> - <?php echo $osw->config['max_password']; ?> characters
+		<input type="password" id="InputPassword" class="form-control" name="password" maxlength="<?php echo $osw->config['max_password']; ?>" required />
 		</div>
 	</div>
 
 	<div class="form-group">
-		<label for="InputCPassword" class="col-sm-3 control-label">Confirm Password</label>
+		<label for="InputCPassword" class="col-sm-3 control-label">Confirm Password <font color="Red">*</font></label>
 		<div class="col-sm-9">
-		<input type="password" id="InputCPassword" name="password_c" maxlength="<?php echo $osw->config['max_password']; ?>" required />
+		<input type="password" id="InputCPassword" class="form-control" name="password_c" maxlength="<?php echo $osw->config['max_password']; ?>" required />
 		</div>
 	</div>
 
 	<div class="form-group">
-		<label for="InputEmail" class="col-sm-3 control-label">Email Address</label>
+		<label for="InputEmail" class="col-sm-3 control-label">Email site_address</label>
 		<div class="col-sm-9">
-		<input type="text" id="InputEmail" name="email" maxlength="100" value="" required />
+		<input type="text" id="InputEmail" class="form-control" name="email" maxlength="100" value="" required />
 		</div>
 	</div>
 <?php
@@ -83,7 +78,8 @@ if ($osw->config['security_image'] == 'yes') {
 	<div class="form-group">
 		<label for="Submit" class="col-sm-3 control-label"></label>
 		<div class="col-sm-9">
-		<input type="submit" id="Submit" value="Register" class="btn btn-primary" />
+		<input type="submit" id="Submit" value="Register" class="btn btn-primary" /><br>
+		<br><small><font color="Red">*</font> Passwords MUST be <?php echo $osw->config['min_password']; ?> to <?php echo $osw->config['max_password']; ?> characters</small>
 		</div>
 	</div>
 
@@ -99,12 +95,11 @@ if ($osw->config['security_image'] == 'yes') {
 This website uses cookies to store login information so the system knows its you without forcing you to login all the time.<br>
 By registering you agree to allow this site place a cookie on your computer.<br>
 Don't worry it doesn't store your password. Just your uuid, the current time and your session id.<br>
-For more information go google Opensimweb.<br>
+For more information google Opensimweb.<br>
 <br>
-When registering you agree to our <a href='<?php echo $address; ?>/tos.php'>Terms of Service</a> and our <a href='<?php echo $address; ?>/privacypolicy.php'>Cookie and Privacy Policy</a><br>
-We only require a valid email address at registration to confirm that you are human and not a spam bot.<br>
+When registering you agree to our <a href='<?php echo $site_address; ?>/tos.php'>Terms of Service</a> and our <a href='<?php echo $site_address; ?>/privacypolicy.php'>Cookie and Privacy Policy</a><br>
+We only require a valid email site_address at registration to confirm that you are human and not a spam bot.<br>
 We wont email you anything else without your approval.<br>
-We also will NOT sell or give your email address to any annoying spam company because we hate spam too.
+We also will NOT sell or give your email site_address to any annoying spam company because we hate spam too.
 </small>
 </p>
-</fieldset>
